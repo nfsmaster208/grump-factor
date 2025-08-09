a2401ea8397b108ef9830c5c7bf801b5786998eb
nfsmaster208 <nfsmaster208@gmail.com>
2025-08-08 22:41:07 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-window.addEventListener('error', e => {
+/*window.addEventListener('error', e => {
   const el = document.createElement('pre'); el.textContent = 'Error: ' + (e.error?.message ?? e.message);
   el.style.cssText='white-space:pre-wrap;padding:12px;margin:12px;background:#fee;border:1px solid #f99;font:12px ui-monospace'; document.body.prepend(el);
 });
 window.addEventListener('unhandledrejection', e => {
   const el = document.createElement('pre'); el.textContent = 'Promise error: ' + String((e as any).reason);
   el.style.cssText='white-space:pre-wrap;padding:12px;margin:12px;background:#fee;border:1px solid #f99;font:12px ui-monospace'; document.body.prepend(el);
-});
+});*/
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
