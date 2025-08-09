a17fea0cdb924bb1d9513eda719daf72302be367
nfsmaster208 <nfsmaster208@gmail.com>
2025-08-08 23:02:38 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(() => {});
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
